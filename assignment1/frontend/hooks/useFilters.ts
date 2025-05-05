863d9f6ea73318297e1aea628ac8f22760fd242f
PaulMue0 <muellerpaul2000@live.de>
2025-05-05 16:24:44 +0200

    chore: cleanup assignment 1. It can still be found under the branch -> old-assignment-1

--- assignment1/frontend/hooks/useFilters.ts
+++
@@ -1,8 +0,0 @@
-export async function fetchFilterOptions() {
-    const res = await fetch("/api/airport_attributes");
-    if (!res.ok) {
-      throw new Error("Failed to fetch filter options");
-    }
-    return res.json();
-  }
-  
