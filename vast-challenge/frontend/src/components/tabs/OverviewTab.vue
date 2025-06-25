49d94fdb9a3bf8168da46a5270fbbc146d63c2a5
PaulMue0 <muellerpaul2000@live.de>
2025-06-25 14:14:28 +0200

    chore: 🧹 cleanup unsused files

--- vast-challenge/frontend/src/components/tabs/OverviewTab.vue
+++ vast-challenge/frontend/src/components/tabs/OverviewTab.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="h-full">
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 p-4">
-      <!-- Signature Charts for Aggregated Data -->
       <OverallSentimentDistribution />
-      <!-- <EntityTypeDistribution /> -->
-      <!-- <EntityActivityCard :entity-id="selectedEntityId || ''" /> -->
       <DatasetNodeComparison />
       <TopicSentimentOverview class="col-span-2" />
     </div>
@@ -14,19 +11,15 @@
 <script lang="ts">
 import { defineAsyncComponent, defineComponent, PropType } from 'vue';
 
-const EntityTypeDistribution = defineAsyncComponent(() => import('../mini-visualizations/EntityTypeDistribution.vue'));
 const OverallSentimentDistribution = defineAsyncComponent(() => import('../mini-visualizations/OverallSentimentDistribution.vue'));
 const TopicSentimentOverview = defineAsyncComponent(() => import('../mini-visualizations/TopicSentimentOverview.vue'));
-const EntityActivityCard = defineAsyncComponent(() => import('../mini-visualizations/EntityActivityCard.vue'));
 const DatasetNodeComparison = defineAsyncComponent(() => import('../mini-visualizations/DatasetNodeComparison.vue'));
 
 export default defineComponent({
   name: 'OverviewTab',
   components: {
-    EntityTypeDistribution,
     OverallSentimentDistribution,
     TopicSentimentOverview,
-    EntityActivityCard,
     DatasetNodeComparison,
   },
   props: {
