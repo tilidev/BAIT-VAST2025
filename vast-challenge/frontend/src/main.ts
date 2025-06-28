100d1450025494d0c7824afd0f3e0efcfb3504cc
PaulMue0 <muellerpaul2000@live.de>
2025-06-28 17:08:47 +0200

    setup: added tooltip directive

--- vast-challenge/frontend/src/main.ts
+++ vast-challenge/frontend/src/main.ts
@@ -5,12 +5,15 @@ import Aura from '@primeuix/themes/aura';
 import 'primeicons/primeicons.css';
 import './style.css';
 import App from './App.vue';
+import Tooltip from 'primevue/tooltip';
 
 const pinia = createPinia();
 const app = createApp(App);
 
 // Use plugins
 app.use(pinia);
+app.directive('tooltip', Tooltip);
+
 app.use(PrimeVue, {
   theme: {
     preset: Aura, // Use the base Aura preset
@@ -23,4 +26,3 @@ app.use(PrimeVue, {
 
 // Mount the app
 app.mount('#app');
-
