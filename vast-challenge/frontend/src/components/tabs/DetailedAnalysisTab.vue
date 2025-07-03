2e6a733ac535a7d5e85dfc116612035b1d71083b
PaulMue0 <muellerpaul2000@live.de>
2025-07-03 15:49:51 +0200

    feat: tabs handle grid

--- vast-challenge/frontend/src/components/tabs/DetailedAnalysisTab.vue
+++ vast-challenge/frontend/src/components/tabs/DetailedAnalysisTab.vue
@@ -16,9 +16,9 @@ export default defineComponent({
   data() {
     return {
       layout: [
-        { x: 0, y: 0, w: 6, h: 8, i: '0', component: 'GraphView' },
-        { x: 6, y: 0, w: 6, h: 8, i: '1', component: 'GraphView' },
-        { x: 0, y: 8, w: 6, h: 8, i: '2', component: 'GraphView' },
+        { x: 0, y: 0, w: 6, h: 8, i: '0', component: 'GraphView', props: { filterKey: 'jo' } },
+        { x: 6, y: 0, w: 6, h: 8, i: '1', component: 'GraphView', props: { filterKey: 'tr' } },
+        { x: 0, y: 8, w: 6, h: 8, i: '2', component: 'GraphView', props: { filterKey: 'fi' } },
         { x: 6, y: 8, w: 6, h: 8, i: '3', component: 'IndustrySentimentBreakdown' },
         { x: 0, y: 16, w: 12, h: 8, i: '4', component: 'PersonSentimentAcrossDatasets' },
         { x: 0, y: 24, w: 12, h: 8, i: '5', component: 'EntitySentimentConsistencyMatrix' },
--- vast-challenge/frontend/src/components/tabs/OverviewTab.vue
+++ vast-challenge/frontend/src/components/tabs/OverviewTab.vue
@@ -16,7 +16,7 @@ export default defineComponent({
   data() {
     return {
       layout: [
-        { x: 0, y: 0, w: 4, h: 4, i: '0', component: 'OverallSentimentDistribution' },
+        { x: 0, y: 0, w: 4, h: 8, i: '0', component: 'OverallSentimentDistribution' },
         { x: 4, y: 0, w: 4, h: 4, i: '1', component: 'DatasetNodeComparison' },
         { x: 0, y: 4, w: 8, h: 6, i: '2', component: 'TopicSentimentOverview' },
         { x: 0, y: 10, w: 12, h: 8, i: '3', component: 'IndustrySimilarityHeatmap' },
