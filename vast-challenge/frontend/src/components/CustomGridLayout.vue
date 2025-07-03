7c23d2c1d0e6f81b9386dfe17c208fb4b7d22e7a
PaulMue0 <muellerpaul2000@live.de>
2025-07-03 16:04:30 +0200

    style: changed bg and icons of grid items

--- vast-challenge/frontend/src/components/CustomGridLayout.vue
+++ vast-challenge/frontend/src/components/CustomGridLayout.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="h-full">
+  <div class="h-full bg-gray-50 dark:bg-gray-900 p-4">
     <div id="content" class="h-full">
+      <!-- eslint-disable-next-line vue/no-v-model-argument -->
       <GridLayout v-model:layout="layout" :col-num="colNum" :row-height="30" :is-draggable="isDraggable"
         :is-resizable="isResizable" :vertical-compact="true" :use-css-transforms="true" class="h-full">
-        <GridItem v-for="item in layout" :key="item.i" :x="item.x" :y="item.y" :w="item.w" :h="item.h" :i="item.i">
-          <span class="remove" @click="removeItem(item.i)">x</span>
-          <div class="h-full w-full">
-            <component :is="item.component" v-bind="item.props" />
+        <GridItem v-for="item in layout" :key="item.i" :x="item.x" :y="item.y" :w="item.w" :h="item.h" :i="item.i"
+          class="bg-white dark:bg-gray-800 rounded-lg shadow-md overflow-hidden transition-all duration-300 ease-in-out hover:shadow-xl">
+          <div class="h-full w-full relative">
+            <button @click="removeItem(item.i)"
+              class="absolute top-2 right-2 z-10 p-1 bg-gray-200 dark:bg-gray-700 rounded-full text-gray-600 dark:text-gray-300 hover:bg-red-500 hover:text-white transition-colors duration-200"
+              aria-label="Remove item">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+            <div class="p-4 h-full w-full">
+              <component :is="item.component" v-bind="item.props" />
+            </div>
           </div>
         </GridItem>
       </GridLayout>
@@ -94,19 +105,18 @@ export default defineComponent({
 
 <style scoped>
 .vue-grid-layout {
-  background: #eee;
+  @apply transition-all duration-500 ease-in-out;
+}
+
+.vue-grid-item.vue-grid-placeholder {
+  @apply bg-blue-200 dark:bg-blue-800 rounded-lg opacity-50;
+  -webkit-transition: all 300ms ease;
+  -moz-transition: all 300ms ease;
+  -o-transition: all 300ms ease;
+  transition: all 300ms ease;
 }
 
 .vue-grid-item:not(.vue-grid-placeholder) {
-  background: #ccc;
-  border: 1px solid black;
-  overflow: hidden;
-}
-
-.vue-grid-item .remove {
-  position: absolute;
-  right: 2px;
-  top: 0;
-  cursor: pointer;
+  @apply border border-gray-200 dark:border-gray-700;
 }
 </style>
