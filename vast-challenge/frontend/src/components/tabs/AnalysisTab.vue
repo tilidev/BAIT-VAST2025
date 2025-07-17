bb8cab4b9343cb1a406c1c614c20425faa6da825
PaulMue0 <muellerpaul2000@live.de>
2025-07-17 02:54:30 +0200

    feat: diff matrix

--- vast-challenge/frontend/src/components/tabs/AnalysisTab.vue
+++ vast-challenge/frontend/src/components/tabs/AnalysisTab.vue
@@ -62,11 +62,7 @@ export default defineComponent({
       datasets: ['jo', 'fi', 'tr'],
       detailColors: { jo: 'black', fi: 'red', tr: 'blue' } as { [key: string]: string },
       layout: [
-
-        // DetailedAnalysisTab layout - with y-offset to avoid overlap
-        { x: 0, y: 0, w: 3, h: 11, i: '6', component: 'GraphView', props: { filterKey: 'jo' } },
-        { x: 3, y: 0, w: 3, h: 11, i: '7', component: 'GraphView', props: { filterKey: 'fi' } },
-        { x: 6, y: 0, w: 3, h: 11, i: '8', component: 'GraphView', props: { filterKey: 'tr' } },
+        { x: 0, y: 0, w: 6, h: 14, i: 'sentiment-diff-matrix', component: 'SentimentDiffMatrix' },
         { x: 9, y: 0, w: 3, h: 11, i: '11', component: 'EntitySentimentConsistencyMatrix' },
         // OverviewTab layout
         { x: 0, y: 10, w: 5, h: 19, i: '5', component: 'EgoNetwork' },
